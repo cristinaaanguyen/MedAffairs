1b6bb4b86ee58b926db9ef0430ac02f6b40e7597
Cristina Nguyen <cristina_nguyen@edwards.com>
2018-09-11 18:42:00 -0700

    adding Movie Browsing project and Medical Affairs

---
+++ MedicalAffairsSample/MedicalAffairsSample/Controllers/MedicalAffairsController.cs
@@ -0,0 +1,25 @@
+﻿using MedicalAffairsSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicalAffairsSample.Controllers
+{
+    public class MedicalAffairsController : Controller
+    {
+        // GET: MedicalAffairs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult MedicalAffairsView()
+        {
+
+            return View();
+        }
+    }
+}
